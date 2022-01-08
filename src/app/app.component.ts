a91054264b78ffa966ddbcc97478c60849348942
Tanuj Ravi Rao <tanujrao99@gmail.com>
2022-01-08 22:39:44 +0530

    feat: Update Angular, Install Nebular, Init Layout
    Signed-off-by: Tanuj Ravi Rao <tanujrao99@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ElectronService } from './core/services';
 import { TranslateService } from '@ngx-translate/core';
 import { APP_CONFIG } from '../environments/environment';
+import { NbMenuItem } from '@nebular/theme';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,14 @@ import { APP_CONFIG } from '../environments/environment';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  public menu: NbMenuItem[] = [
+    {
+      title: 'Home',
+      link: '/home',
+      icon: 'home-outline',
+    },
+  ];
+
   constructor(
     private electronService: ElectronService,
     private translate: TranslateService
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
@@ -15,12 +15,29 @@ import { HomeModule } from './home/home.module';
 import { DetailModule } from './detail/detail.module';
 
 import { AppComponent } from './app.component';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+import {
+  NbThemeModule,
+  NbSidebarModule,
+  NbMenuModule,
+  NbDatepickerModule,
+  NbIconModule,
+  NbSelectModule,
+  NbUserModule,
+  NbActionsModule,
+  NbContextMenuModule,
+  NbSearchModule,
+  NbLayoutModule,
+  NbButtonModule
+} from '@nebular/theme';
+import { HeaderComponent } from './partials/header/header.component';
+import { FooterComponent } from './partials/footer/footer.component';
 
 // AoT requires an exported function for factories
-const httpLoaderFactory = (http: HttpClient): TranslateHttpLoader =>  new TranslateHttpLoader(http, './assets/i18n/', '.json');
+const httpLoaderFactory = (http: HttpClient): TranslateHttpLoader => new TranslateHttpLoader(http, './assets/i18n/', '.json');
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent],
   imports: [
     BrowserModule,
     FormsModule,
@@ -30,6 +47,21 @@ const httpLoaderFactory = (http: HttpClient): TranslateHttpLoader =>  new Transl
     HomeModule,
     DetailModule,
     AppRoutingModule,
+    NbThemeModule.forRoot({ name: 'default' }),
+    NbSidebarModule.forRoot(),
+    NbMenuModule.forRoot(),
+    NbDatepickerModule.forRoot(),
+    NbIconModule,
+    NbSelectModule,
+    NbUserModule,
+    NbActionsModule,
+    NbContextMenuModule,
+    NbSearchModule,
+    NbLayoutModule,
+    NbEvaIconsModule,
+    NbButtonModule,
+    ReactiveFormsModule,
+
     TranslateModule.forRoot({
       loader: {
         provide: TranslateLoader,
@@ -41,4 +73,4 @@ const httpLoaderFactory = (http: HttpClient): TranslateHttpLoader =>  new Transl
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
